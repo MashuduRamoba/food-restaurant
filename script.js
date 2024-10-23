85c56bbd22747469ae6ece57c859e6c4544f0c11
MashuduRamoba <119120286+MashuduRamoba@users.noreply.github.com>
2024-10-23 13:51:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function unCheckCheckbox(){
+    document.getElementById("check").check=false;
+}
+
+document.addEventListener("DOMContentLoaded",function() {
+    setTimeout(function(){
+        document.querySelector(".fadeInOnLoad").style.opacity=1;},100);
+    })
+    ;
+
+
